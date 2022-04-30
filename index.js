642ea015b4d976f683570a1b158d8ba05195f887
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-04-30 16:57:28 +0200

    footer bigger screens

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ function openMenu (){
         menu.style.display = "block"
     }
 }
+
 console.log(menu);
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.logo', {
+    delay: 500
+});
+
+ScrollReveal().reveal('.hero__title', {
+    delay: 600,
+    origin: 'left'
+});
